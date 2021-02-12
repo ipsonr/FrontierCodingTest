8fb089b9fd25d7dc2a83a4d7beb5fe3c8475f851
Ron Ipson <33043121+ipsonr@users.noreply.github.com>
2021-02-11 17:44:20 -0700

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using FrontierCodingTest.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace FrontierCodingTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<AccountModel> Accounts { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Accounts = GetAccounts();
+        }
+
+        private List<AccountModel> GetAccounts()
+        {
+            List<AccountModel> accounts = new List<AccountModel>();
+            string myUrl = "https://frontiercodingtests.azurewebsites.net/api/accounts/getall";
+
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(myUrl).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content;
+
+                    string responseString = responseContent.ReadAsStringAsync().Result;
+                    accounts = JsonConvert.DeserializeObject<List<AccountModel>>(responseString);
+                }
+            }
+                return accounts;
+        }
+    }
+}
